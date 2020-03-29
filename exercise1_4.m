7e2f49ba88d7f23196732f8d7a98abf0332b117a
Victor Timofei <victor@vtimothy.com>
2020-03-29 18:49:33 +0300

    Based on the aspect ratio each image belongs to a bin, and computed  Prior Probabilities and Likelihoods

--- exercise1_4.m
+++ exercise1_4.m
@@ -1,3 +1,4 @@
+clear ; close all; clc
 data_file = '.\data\mnist.mat';
 
 data = load(data_file);
@@ -9,7 +10,6 @@ for i = 1:size(data.trainX, 1)
     images(i, :, :) = reshape(img, 28, 28)';
     labels(i) = data.trainY(i);
 end
-test = images (72,:,:)
 digit_C1_indices = find(labels == 1); % digit 1
 digit_C2_indices = find(labels == 2); % digit 2
 
@@ -28,20 +28,55 @@ maxAspectRatio = max(aRatios)
 numBins = 3;
 
 binEnds = linspace(minAspectRatio, maxAspectRatio, numBins+1);
-
 C1_bins = zeros(numBins, 1);
 C2_bins = zeros(numBins, 1);
 all_bins = zeros(numBins, 1);
-
+for i = 1:size(data.trainX, 1)
+    bin = findBin(aRatios(i),binEnds);
+    if bin == 1
+        all_bins(1) = all_bins(1) + 1;
+    elseif bin == 2
+        all_bins(2) = all_bins(2) + 1;
+    elseif bin ==3
+        all_bins(3) = all_bins(3) + 1;
+    end
+    bins(i) = bin;
+    if labels(i) == 1
+        if bin == 1
+            C1_bins(1) = C1_bins(1) + 1;
+        elseif bin == 2
+            C1_bins(2) = C1_bins(2) + 1;
+        elseif bin ==3
+            C1_bins(3) = C1_bins(3) + 1;
+        end
+        bins_C1(i) = bin;
+    end
+    if labels(i) == 2
+        if bin == 1
+            C2_bins(1) = C2_bins(1) + 1;
+        elseif bin == 2
+            C2_bins(2) = C2_bins(2) + 1;
+        elseif bin ==3
+            C2_bins(3) = C2_bins(3) + 1;
+        end
+        bins_C2(i) = bin;
+    end
+end
+figure
+hist(bins_C1,3)
+figure
+hist(bins_C2,3)
+figure
+hist(bins,3)
 % Use the findBin function to get the counts for the histogram
 
 % Prior Probabilities
-%PC1 = ...
-%PC2 = ...
+PC1 = length(bins_C1)/length(labels);
+PC2 = length(bins_C2)/length(labels);
 
 % Likelihoods
-%PgivenC1 = ...
-%PgivenC2 = ...
+PgivenC1 = C1_bins/length(bins_C1);
+PgivenC2 = C2_bins/length(bins_C2);
 
 % Evidence 
 
--- findBin.m
+++ findBin.m
@@ -1,4 +1,12 @@
 function index = findBin(value, binEnds)
     numBins = length(binEnds) - 1;
+    
+    index = numBins;
+    for i = 1:numBins
+       if value>binEnds(i)
+          index = i;
+       end
+    end
+    
     % return the index of the bin where value belongs {1, ..., numBins}. 
 end
