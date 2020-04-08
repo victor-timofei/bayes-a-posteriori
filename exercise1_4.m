f9bac088de1a8ae5f773bcb9cae7250b5c32ea15
Victor Timofei <victor@vtimothy.com>
2020-04-08 19:46:24 +0300

    Computed Posterior Probabilities

--- exercise1_4.m
+++ exercise1_4.m
@@ -32,15 +32,17 @@ C1_bins = zeros(numBins, 1);
 C2_bins = zeros(numBins, 1);
 all_bins = zeros(numBins, 1);
 for i = 1:size(data.trainX, 1)
-    bin = findBin(aRatios(i),binEnds);
-    if bin == 1
-        all_bins(1) = all_bins(1) + 1;
-    elseif bin == 2
-        all_bins(2) = all_bins(2) + 1;
-    elseif bin ==3
-        all_bins(3) = all_bins(3) + 1;
+    if labels(i) == 1 || labels(i) == 2
+        bin = findBin(aRatios(i),binEnds);
+        if bin == 1
+            all_bins(1) = all_bins(1) + 1;
+        elseif bin == 2
+            all_bins(2) = all_bins(2) + 1;
+        elseif bin ==3
+            all_bins(3) = all_bins(3) + 1;
+        end
+        bins(i) = bin;
     end
-    bins(i) = bin;
     if labels(i) == 1
         if bin == 1
             C1_bins(1) = C1_bins(1) + 1;
@@ -71,15 +73,16 @@ hist(bins,3)
 % Use the findBin function to get the counts for the histogram
 
 % Prior Probabilities
-PC1 = length(bins_C1)/length(labels);
-PC2 = length(bins_C2)/length(labels);
+PC1 = length(bins_C1)/length(bins);
+PC2 = length(bins_C2)/length(bins);
 
 % Likelihoods
 PgivenC1 = C1_bins/length(bins_C1);
 PgivenC2 = C2_bins/length(bins_C2);
 
 % Evidence 
+Pevidence = all_bins/length(bins);
 
 % Posterior Probabilities
-%PC1givenL = ...
-%PC2givenL = ...
+PC1givenL = (PgivenC1(1) * PC1)/Pevidence(1);
+PC2givenL = (PgivenC2(1) * PC2)/Pevidence(1);
